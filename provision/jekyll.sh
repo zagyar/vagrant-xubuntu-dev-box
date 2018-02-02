5781a6f4270d52852d96c65b2a3149d0f6f62a08
throthe <nomad.blow@gmail.com>
2018-02-02 16:59:03 +0800

    correct some errors in ruby script and add jekyll

---
+++ provision/jekyll.sh
@@ -0,0 +1,5 @@
+
+echo 'Install Jekyll with gem ... '
+sudo gem install jekyll
+
+echo -n "Jekyll Version: ";  jekyll --version
--- provision/ruby.sh
+++ provision/ruby.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 # Install Ruby + RubyGems
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install ruby
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install ruby ruby-dev
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install rubygems
+
+# Display the versions 
+echo -n "Ruby Version: ";ruby -v
+echo -n "RubyGems Version: ";gem -v
