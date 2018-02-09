a6ee8ae6bab1624c9a1538b918fbf10bf0d0cbee
throthe <nomad.blow@gmail.com>
2018-02-09 12:59:26 +0800

    add initial go lang support

---
+++ provision/lang/go/core.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+# Add Repo
+sudo add-apt-repository -y ppa:gophers/archive
+# Update apt-get
+sudo apt-get update
+
+sudo apt-get install golang-1.9-go
+
+# !! Note that golang-1.9-go puts binaries in /usr/lib/go-1.9/bin. 
+# !! If you want them on your PATH, you need to make that change yourself 
+
