7a2483ec79eb61467596ce805b7f084455e974ae
throthe <nomad.blow@gmail.com>
2018-02-09 13:21:08 +0800

    changes sudo install

--- provision/lang/go/core.sh
+++ provision/lang/go/core.sh
@@ -6,7 +6,7 @@ sudo add-apt-repository -y ppa:gophers/archive
 # Update apt-get
 sudo apt-get update
 
-sudo apt-get install golang-1.9-go
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y golang-1.9-go
 
 # !! Note that golang-1.9-go puts binaries in /usr/lib/go-1.9/bin. 
 # !! If you want them on your PATH, you need to make that change yourself 
