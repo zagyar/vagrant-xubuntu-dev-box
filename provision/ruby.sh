0798b2db3dd4c1484d1c0902748f8c8eaa1b8f02
throthe <nomad.blow@gmail.com>
2018-02-20 10:06:40 +0800

    change -v to --version to make it more generally

--- provision/ruby.sh
+++ provision/ruby.sh
@@ -5,5 +5,5 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y install ruby ruby-dev
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install rubygems
 
 # Display the versions 
-echo -n "Ruby Version: ";ruby -v
-echo -n "RubyGems Version: ";gem -v
+echo -n "Ruby Version: ";ruby --version
+echo -n "RubyGems Version: ";gem --version
