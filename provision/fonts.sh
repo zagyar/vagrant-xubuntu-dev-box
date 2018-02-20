0d4c5b9ee313ce2a408f79db178c5395db7bce5b
throthe <nomad.blow@gmail.com>
2018-02-20 11:24:59 +0800

    supress error for overwritting interaction for overwriting

--- provision/fonts.sh
+++ provision/fonts.sh
@@ -6,8 +6,8 @@ sudo mkdir -p /usr/local/share/fonts/truetype/open-sans
 # download font
 curl -fsSL -o /tmp/open-sans.zip https://www.fontsquirrel.com/fonts/download/open-sans
 
-# unzip
-sudo unzip -q /tmp/open-sans.zip -d /usr/local/share/fonts/truetype/open-sans
+# unzip (-qq = quiter, -o = overwrite)
+sudo unzip -qq -o /tmp/open-sans.zip -d /usr/local/share/fonts/truetype/open-sans
 
 # remove temp file
 sudo rm -f /tmp/open-sans.zip
