a95b57bacd0b2ffec46a32a751aedc0dedb8f733
throthe <nomad.blow@gmail.com>
2018-02-20 09:25:26 +0800

    add MariaDB server script

---
+++ provision/db/mariadb.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# MariaDB Server is one of the most popular database servers in the world. 
+# It’s made by the original developers of MySQL and guaranteed to stay open source. 
+# Notable users include Wikipedia, WordPress.com and Google.
+# Website: https://mariadb.org/
+
+
+# Here are the commands to run to install MariaDB on your Ubuntu system:
+sudo apt-get install software-properties-common
+# Get the key for trusted package authentication
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+# Add MariaDB PPA
+sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://suro.ubaya.ac.id/mariadb/repo/10.2/ubuntu xenial main'
+
+# Once the key is imported and the repository added you can install MariaDB with:
+sudo apt update
+# Install MariaDB
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install mariadb-server
