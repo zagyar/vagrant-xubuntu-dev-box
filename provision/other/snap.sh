521c5c1107e7d2476b42542a0509e059e4010775
throthe <nomad.blow@gmail.com>
2018-02-20 09:11:21 +0800

    add snapcraft package manager

---
+++ provision/other/snap.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Package Manager for containerised software packages that are simple to create and install. 
+# They auto-update and are safe to run. And because they bundle their dependencies, 
+# they work on all major Linux systems without modification.
+# Website: https://snapcraft.io/
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install snapd
